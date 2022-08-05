17ea34d3bd74a25cc69d625b0425bb97c1da89b9
GAV07 <egavin1992@gmail.com>
2022-08-04 22:11:03 -0400

    add link animation and scroll packages

--- components/miamiData.js
+++ components/miamiData.js
@@ -9,7 +9,11 @@
                 <dl className="divide-y divide-white">
                   {section.fields.list.map((point, i) => (
                     <div key={point.sys.id} className="pt-6 pb-8 md:grid md:grid-cols-12 md:gap-8">
-                      <dt className="text-base font-medium text-white md:col-span-5"><a className="hover:underline" href={point.fields.url}>{point.fields.title}</a></dt>
+                      <dt className="text-base font-medium text-white md:col-span-5">
+                        <a className="link link--elara" href={point.fields.url}>
+                          <span>{point.fields.title}</span>
+                        </a>
+                      </dt>
                       <dd className="mt-2 md:mt-0 md:col-span-7">
                         <p className="text-base text-white">{point.fields.description}</p>
                       </dd>
--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,6 @@
 import '../styles/index.css'
+import '../styles/data-links.css'
+import "animate.css/animate.min.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
