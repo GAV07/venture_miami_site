bb1c393a510725a41127f8ce894793081cee5a3c
Erick Gavin <gavin@Ericks-MBP.attlocal.net>
2022-10-26 18:45:36 -0400

    add registration link

--- components/banner.js
+++ components/banner.js
@@ -30,7 +30,7 @@ export default function Banner() {
                     </div>
                     <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
                         <a
-                        href="/hiring-fairhttps://www.eventbrite.com/e/403469507157"
+                        href="https://www.eventbrite.com/e/venture-miami-tech-hiring-fair-3-tickets-403469507157"
                         className="flex items-center justify-center rounded-md border border-transparent bg-vm-blue px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-vm-accent hover:text-black"
                         >
                         Register Now
