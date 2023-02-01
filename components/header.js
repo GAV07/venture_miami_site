769e5df60188f12b72b743923e573a147cabf9a7
supER sonICK <egavin1992@gmail.com>
2023-02-01 15:32:21 -0500

    fix header

--- components/header.js
+++ components/header.js
@@ -260,7 +260,7 @@ export default function Header() {
               Relocating to Miami?
             </a>
             <a
-              href="/initatives"
+              href="/initiatives"
               className="text-base font-medium text-gray-500 hover:text-gray-900"
             >
               Our Initiatives
