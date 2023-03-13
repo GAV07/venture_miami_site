b1ea626028125bd4d4f61bc41560df8b706e3598
supER sonICK <egavin1992@gmail.com>
2023-03-13 10:25:49 -0400

    remove data modules

--- components/data/TalentTab.js
+++ components/data/TalentTab.js
@@ -1,15 +1,15 @@
-import { PieDash } from './charts/PieChart'
-import { Stats } from './Stats'
+// import { PieDash } from './charts/PieChart'
+// import { Stats } from './Stats'
 
-export function TalentTab({data}) {
-    console.log(data)
-    return (
-        <div className="flex flex-col">
-            <Stats data={data}/>
-            <div className="w-64 h-64 p-10 bg-white rounded shadow">
-                <PieDash />
-            </div>
-        </div>
+// export function TalentTab({data}) {
+//     console.log(data)
+//     return (
+//         <div className="flex flex-col">
+//             <Stats data={data}/>
+//             <div className="w-64 h-64 p-10 bg-white rounded shadow">
+//                 <PieDash />
+//             </div>
+//         </div>
 
-    )
-}
+//     )
+// }
--- components/home/Banner.js
+++ components/home/Banner.js
@@ -23,18 +23,18 @@ export default function Banner(props) {
                     </div>
                     <div className="order-3 mr-2 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
                         <a
-                        href="https://www.eventbrite.com/e/emerge-americas-silicon-valley-bank-black-history-month-pitch-competition-tickets-493023926837"
+                        href="https://www.eventbrite.com/e/venture-miami-tech-hiring-fair-4-and-talent-summit-tickets-568374271747?aff=erelexpmlt"
                         className="flex items-center justify-center rounded-md border border-transparent bg-vm-accent px-4 py-2 text-sm font-medium text-vm-blue shadow-sm hover:bg-white hover:text-black"
                         >
-                        Attend
+                        Attend Event
                         </a>
                     </div>
                     <div className="order-3 mt-2 w-full flex-shrink-0 sm:order-2 sm:mt-0 sm:w-auto">
                         <a
-                        href="/pitch"
+                        href="/hiring-fair"
                         className="flex items-center justify-center rounded-md border border-transparent bg-vm-blue px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-vm-accent hover:text-black"
                         >
-                        Apply Now
+                        More Info
                         </a>
                     </div>
                     <div className="self-start order-2 flex-shrink-0 sm:order-3 sm:ml-2">
