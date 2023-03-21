87a70d5b6bf63e03ac41b23e607d8e2ca9ed66e4
supER sonICK <egavin1992@gmail.com>
2023-03-21 12:15:03 -0400

    clean up airtable requests

--- pages/initiatives.js
+++ pages/initiatives.js
@@ -1,8 +1,8 @@
 import { Container } from '../components/container'
 import { Hero } from '../components/talent/Hero'
 import { Data } from '../components/talent/Data'
-import { Companies } from '../components/talent/Companies'
-import { getTalent } from "../lib/airtable"
+//import { Companies } from '../components/talent/Companies'
+//import { getTalent } from "../lib/airtable"
 import Layout from '../components/layout'
 import Head from 'next/head'
 import { Client } from '../lib/contentful'
@@ -31,14 +31,13 @@ export async function getStaticProps() {
   const tabs = await Client.getEntry('30iQqtglBuXGa77V3dZL5J')
   const data = await Client.getEntry('27Tb8oRqA4rOI4T5IUtUBn', {include: 3})
   const footer = await Client.getEntry('6ismKzbJGVMc3w7KWoEvfA')
-  const records = await getTalent("Confirmed Companies");
+  //const records = await getTalent("Confirmed Companies");
   
   return {
     props: {
       hero: hero.fields,
       tabs: tabs.fields,
       data: data.fields,
-      companies: records,
       footer: footer.fields
     }
   }
