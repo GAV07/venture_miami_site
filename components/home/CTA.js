a71ea9c3e6a4457be35825170d2f3243e39bf79d
Ernesto Louis <elouis12@me.com>
2023-10-24 13:53:25 -0400

    removed old overlay

--- components/home/CTA.js
+++ components/home/CTA.js
@@ -23,41 +23,6 @@ export function CTA(props) {
         <div className={"w-full m-auto pt-16"}>
 
             <div className={"w-[90%] m-auto xl:w-[85%]"}>
-                {/*<div className={"relative z-[1] m-auto bg-[#161A1D] rounded-[16px]"}>*/}
-             {/*   <div className={"relative z-[1] m-auto bg-[#161A1D] rounded-[16px] bg-[url('https://placekitten.com/1400')] bg-cover"}>
-
-                    <img className={"absolute z-[1] rounded-[8px] w-full h-full object-cover absolute z-[1] top-0 left-0 h-full"} src="https://images.unsplash.com/photo-1652109348316-2ec63fa43632?auto=format&fit=crop&q=60&w=900&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MjZ8fDRrJTIwaGQlMjBpbWFnZXxlbnwwfHwwfHx8MA%3D%3D" alt=""/>
-
-                    <div className="relative z-[1] font-bold flex flex-col justify-center items-center ml-auto mr-auto p-[80px]">
-                        <h2 className="text-white text-[36px] text-center">
-                            {props.content.title}
-                        </h2>
-                        <p className="text-[#9CA6B6] text-[18px] text-center">
-                            {props.content.subtitle}
-                        </p>
-                        <div
-                            className="mt-10 flex items-center justify-center gap-x-6 lg:justify-start flex flex-col space-y-5 sm:flex-row sm:justify-stretch flex-1 sm:space-y-0">
-
-                            {
-                                props.content.buttons.map((button, index) => {
-
-                                    return (
-                                        <button key={index}
-                                                className={`rounded-full ${textColor[index]} ${bgColor[index]} px-4 py-4 text-sm font-semibold shadow-sm hover:${bgColor[index]}/100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white`}>
-                                            <a href={button.fields.url}
-                                               className={"flex justify-between items-center w-full gap-x-10"}>
-                                                <p>{button.fields.text}</p>
-                                                <BsArrowRight size={20} fontWeight={20}/>
-                                            </a>
-                                        </button>
-
-                                    );
-                                })
-                            }
-
-                        </div>
-                    </div>
-                </div>*/}
 
                 <div className={`rounded-[8px] relative right-0 flex justify-between items-center z-[2] w-full ease-in duration-300`}>
 
