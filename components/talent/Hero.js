d93c82d1d48250ee08620027050c04081e0f522e
Ernesto Louis <elouis12@me.com>
2023-12-13 18:45:49 -0500

    updated hero image for initiatives page from contentful

--- components/talent/Hero.js
+++ components/talent/Hero.js
@@ -12,7 +12,7 @@ export function Hero(props) {
     <HeroComponent
         title={props.content.title}
         subtitle={props.content.subtitle}
-        imageURL={"https://images.unsplash.com/photo-1507608869274-d3177c8bb4c7?q=80&w=870&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"}
+        imageURL={props.content.backgroundImage.fields.file.url}
     />
 
     )
